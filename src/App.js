ddb1f3d72d781eae5495047d8362986553da6fc7
Devon Hamill <devonhamill93@gmail.com>
2024-04-17 14:15:36 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h3>Navbar</h3>
+        )
+    }
+}
+
+export default Navbar;
