22fe3c1cc4fe9439c262cf0b50c8d88a029cc824
Devon Hamill <devonhamill93@gmail.com>
2024-04-17 14:51:51 -0400

    Router set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+const Home = () => {
+  return <h2>Home</h2>;
+}
+
+const Converter = () => {
+  return <h2>Converter</h2>;
+}
+
 const App = () => {
   return (
     <>
-      <Navbar></Navbar>
-      <Footer></Footer>
+      <Router>
+        <nav class="text-center">
+          <Link to="/">Home</Link>
+          <Link to="/Converter/">Converter</Link>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/Converter/" component={Converter} />
+    </Router>
+    <Footer />
     </>
   );
 }
